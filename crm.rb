0eb7f90f4186bf03b69de1f60022280fe0768ac8
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-19 14:53:49 -0400

    new SQL database created

--- crm.rb
+++ crm.rb
@@ -1,8 +1,24 @@
-require_relative('contact.rb')
 require_relative('rolodex.rb')
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
 $rolodex= Rolodex.new
+
+class Contact
+	include DataMapper::Resource #module this class will represent a single database table 
+
+	property :id, Serial 
+	property :first_name, String
+	property :last_name, String
+	property :email, String
+	property :date, Date
+	property :notes, Text 
+end
+
+DataMapper.finalize #validate any issues with the properties or tables we defined
+DataMapper.auto_upgrade! #effecting any changes to the underlying structure of the tables or columns 
 
 get "/index" do
 	@crm_app_name = "My CRM"
@@ -11,6 +27,7 @@ get "/index" do
 end
 
 get "/contacts" do
+	@contacts = Contact.all
 	erb :contacts
 end
 
@@ -28,9 +45,14 @@ get "/contacts/:id" do
 end
 
 post "/contacts" do
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:date], params[:notes])
-	$rolodex.add_contact(new_contact)
-	redirect to('/contacts')
+	contact = Contact.create(
+			:first_name => params[:first_name],
+			:last_name => params[:last_name],
+			:email => params[:email],
+			:date => params[:date],
+			:notes => params[:notes]
+		)
+	redirect to("/contacts")
 end
 
 get "/contacts/:id/edit" do
--- rolodex.rb
+++ rolodex.rb
@@ -1,5 +1,3 @@
-require_relative("contact.rb")
-	
 class Rolodex
 	attr_reader :contacts
 
